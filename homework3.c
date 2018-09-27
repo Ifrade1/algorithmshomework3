f2e2da9ee2731d7b8017ceb8194122f3f832a58d
Ifrade1 <33909806+Ifrade1@users.noreply.github.com>
2018-09-27 19:53:17 -0400

    Update homework3.c

--- homework3.c
+++ homework3.c
@@ -4,6 +4,6 @@ int main(void) {
     int n;//number the user inputs
     while(scanf("%d", &n) !=EOF){ //scans until end of file
     printf("%d\n", 3*n+7, "\n"); //spits out the function's result
-
+    }
 }
 
