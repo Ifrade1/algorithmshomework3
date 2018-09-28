2dbc057024a1e38373e5ae255a44e17daa5e3803
Gabscurioshop <33849234+Gabscurioshop@users.noreply.github.com>
2018-09-28 13:47:44 -0400

    Update PA0func.c

--- PA0func.c
+++ PA0func.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 #include <PA0make.h>
 
-void myPrintPa0
+void myPrintPA0(void){
+  int n;//number the user inputs
+  while(scanf("%d", &n) !=EOF){//scans inputs until it reaches the end of the file
+    printf("%d\n",3*n+7,"\n");//prints the output
+  }//end of while loop
+  return;
+}
