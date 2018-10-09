dcf70e90a0880aab13b132306e7f5ee6d257e694
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2018-10-09 17:13:34 -0400

    Add files via upload

---
+++ h3/homework3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int  main() {
+
+    int n;//number the user inputs
+    while(scanf("%d", &n) !=EOF){ //scans until end of file
+    printf("%d\n", 3*n+7); //spits out the function's result
+    }
+}
+
